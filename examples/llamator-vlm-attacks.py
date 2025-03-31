95b23e618ed4c12f62fd2f91db847ce469cd3c16
ti3c2 <ti3c2@yandex.com>
2025-03-31 09:36:22 +0300

    Add script with vlm attacks examples

---
+++ examples/llamator-vlm-attacks.py
@@ -0,0 +1,70 @@
+import os
+
+from dotenv import load_dotenv
+
+import llamator
+from llamator.attacks.vlm_m_attack import TestVlmMAttack
+from llamator.attacks.vlm_text_hallucination import TestVlmTextHallucination
+from llamator.client.specific_chat_clients import ClientOpenAI
+
+load_dotenv(".env")  # example of environment variables in the .env.example file
+
+
+client_tested = ClientOpenAI(
+    api_key=os.environ["OPENAI_API_KEY"], base_url="https://api.openai.com/v1/", model="gpt-4o"
+)
+
+client_attack = ClientOpenAI(
+    api_key=os.environ["OPENAI_API_KEY"], base_url="https://api.openai.com/v1/", model="gpt-4o"
+)
+
+
+# Test vision client
+messages_test_vision = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "What's in this image?"},
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+            },
+        ],
+    }
+]
+
+# out = client_tested.interact(history=[], messages=messages_test_vision)
+# print(out)
+
+tests_with_attempts = [
+    # ("complimentary_transition", 3),
+    # ("ethical_compliance", 2),
+    # ("linguistic_evasion", 2),
+    # ("harmful_behavior_multistage", 2),
+]
+
+custom_tests_with_attempts = [
+    # (TestVlmTextHallucination, 3),
+    (TestVlmMAttack, 10),
+]
+
+config = {
+    "enable_logging": True,  # Enable logging
+    "enable_reports": True,  # Enable report generation
+    "artifacts_path": "./artifacts",  # Path to the directory for saving artifacts
+    "debug_level": 2,  # Logging level: 0 - WARNING, 1 - INFO, 2 - DEBUG
+    "report_language": "en",  # Report language: 'en', 'ru'
+}
+
+llamator.start_testing(
+    attack_model=client_attack,
+    tested_model=client_tested,
+    config=config,
+    tests_with_attempts=tests_with_attempts,
+    custom_tests_with_attempts=custom_tests_with_attempts,
+    multistage_depth=3,
+    num_threads=3,
+)
+
