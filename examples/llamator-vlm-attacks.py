ac0022cf8b11a26219a4f5ea6c2191b937deffeb
ti3c2 <ti3c2@yandex.com>
2025-04-20 19:30:56 +0300

    Update examples

--- examples/llamator-vlm-attacks.py
+++ examples/llamator-vlm-attacks.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv
 
 import llamator
-from llamator.attacks.vlm_lowres_docs import TestVlmLowresPdf
+
+# from llamator.attacks.vlm_lowres_docs import TestVlmLowresPdf
 from llamator.attacks.vlm_m_attack import TestVlmMAttack
-from llamator.attacks.vlm_text_hallucination import TestVlmTextHallucination
+
+# from llamator.attacks.vlm_text_hallucination import TestVlmTextHallucination
 from llamator.client.specific_chat_clients import ClientOpenAI
 
 load_dotenv(".env")
@@ -14,15 +16,13 @@ load_dotenv(".env")
 base_url_tested = "https://api.openai.com/v1/"
 # base_url_tested = "http://localhost:7113/v1"
 
-model_tested = "gpt-4o"
+model_tested = "gpt-4o-mini"
 # model_tested = "llava-hf/llava-v1.6-mistral-7b-hf"
 # model_tested = "OpenGVLab/InternVL2_5-8B-MPO"
 # model_tested = "unsloth/Llama-3.2-11B-Vision-Instruct"
 
 client_tested = ClientOpenAI(
-    api_key=os.environ["OPENAI_API_KEY"],  # fmt: skip
-    base_url=base_url_tested,
-    model=model_tested
+    api_key=os.environ["OPENAI_API_KEY"], base_url=base_url_tested, model=model_tested  # fmt: skip
 )
 
 client_attack = ClientOpenAI(
@@ -50,17 +50,9 @@ messages_test_vision = [
 # print(out)
 # exit()
 
-tests_with_attempts = [
-    # ("complimentary_transition", 3),
-    # ("ethical_compliance", 2),
-    # ("linguistic_evasion", 2),
-    # ("harmful_behavior_multistage", 2),
-]
-
-custom_tests_with_attempts = [
-    (TestVlmTextHallucination, 3),
-    # (TestVlmLowresPdf, 100),
-    # (TestVlmMAttack, 90),
+test_params = [
+    ("vlm_m_attack", {"num_attempts": 3, "attack_source": "huggingface"}),
+    # ("system_prompt_leakage", {"num_attempts": 2, "multistage_depth": 3}),
 ]
 
 config = {
@@ -75,8 +67,5 @@ llamator.start_testing(
     attack_model=client_attack,
     tested_model=client_tested,
     config=config,
-    tests_with_attempts=tests_with_attempts,
-    custom_tests_with_attempts=custom_tests_with_attempts,
-    multistage_depth=3,
-    num_threads=3,
+    basic_tests=test_params,
 )
